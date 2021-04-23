7e873f586ff1f44914f479140564ff8e9c794381
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-04-22 22:17:53 -0400

    kiev: Build libgui_vendor
    - Needed for libmotaudioutils.so & libspeakerbundle.so...
    
    Change-Id: If00926b4dba80c0cb0543065cd33de742b2815b2

--- device.mk
+++ device.mk
@@ -34,6 +34,9 @@ PRODUCT_PACKAGES += \
 	NoCutoutOverlay
 
 # Audio
+PRODUCT_PACKAGES += \
+    libgui_vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
