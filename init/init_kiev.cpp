fb4f35c3030dea06b1455e5e72603a59a7a2bed1
Andrew Hexen <SyberHexen@gmail.com>
2021-07-22 12:06:00 -0700

    WIP|kiev: SEPOL
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- init/init_kiev.cpp
+++ init/init_kiev.cpp
@@ -47,6 +47,7 @@ std::vector<std::string> ro_props_default_source_order = {
     "odm.",
     "product.",
     "system.",
+    "system_ext.",
     "vendor.",
 };
 
@@ -85,37 +86,25 @@ void vendor_load_device_properties()
         property_override_device(prop_name.c_str(), value.c_str(), false);
     };
 
-    const auto set_ro_vendor_prop = [](const std::string &source,
-            const std::string &prop, const std::string &value) {
-        auto prop_name = "ro.vendor." + source + prop;
-        property_override_device(prop_name.c_str(), value.c_str(), false);
-    };
-
     bootsku = GetProperty("ro.boot.hardware.sku", "");
     if (bootsku == "XT2113-2") {
-        /* Motorola One 5G Ace */
         for (const auto &source : ro_props_default_source_order) {
             set_ro_boot_prop(source,    "product.hardware.sku", "np");
+            set_ro_product_prop(source, "device", "kiev");
             set_ro_product_prop(source, "model", "motorola one 5G ace");
-            set_ro_product_prop(source, "odm.model", "motorola one 5G ace");
-            set_ro_product_prop(source, "product.model", "motorola one 5G ace");
-            set_ro_product_prop(source, "vendor.model", "motorola one 5G ace");
-            set_ro_vendor_prop(source,  "vendor.product.display", "motorola one 5G ace");
         }
     } else if (bootsku == "XT2113-3") {
         /* Moto G 5G (Unlocked) */
         for (const auto &source : ro_props_default_source_order) {
             set_ro_boot_prop(source,    "product.hardware.sku", "dn");
+            set_ro_product_prop(source, "device", "kiev");
         }
     } else if (bootsku == "XT2113-5") {
         /* Motorola One 5G Ace (AT&T) */
         for (const auto &source : ro_props_default_source_order) {
             set_ro_boot_prop(source,    "product.hardware.sku", "n");
+            set_ro_product_prop(source, "device", "kiev");
             set_ro_product_prop(source, "model", "motorola one 5G ace");
-            set_ro_product_prop(source, "odm.model", "motorola one 5G ace");
-            set_ro_product_prop(source, "product.model", "motorola one 5G ace");
-            set_ro_product_prop(source, "vendor.model", "motorola one 5G ace");
-            set_ro_vendor_prop(source,  "vendor.product.display", "motorola one 5G ace");
         }
     }
     device = GetProperty("ro.product.device", "");
